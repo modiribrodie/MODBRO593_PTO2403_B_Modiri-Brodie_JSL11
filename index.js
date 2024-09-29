d2bbf4288bfbce618e10541317c7b7745874a6d2
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-09-29 20:59:27 +0200

    Theme
    this code checks the current theme and switches it accordingly

--- index.js
+++ index.js
@@ -262,8 +262,15 @@ function toggleSidebar(show) {
 
 function toggleTheme() {
  const body = document.body;
+
+if (body.classList.contains ('dark-theme')) {
+  body.classList.remove('dark-theme'); 
+        body.classList.add('light-theme');
+    } else {
+      body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    }
 }
-if()
 
 
 
